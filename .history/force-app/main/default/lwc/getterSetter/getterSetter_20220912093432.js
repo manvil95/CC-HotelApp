23eb404731a89666a497deab3e226bad818d8392
Manuel Villen <manuvillen@gmail.com>
2024-06-19 19:28:41 +0200

    Delete history folder

--- .history/force-app/main/default/lwc/getterSetter/getterSetter_20220912093432.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class GetterSetter extends LightningElement {
-
-    handleChange
-
-}
--- .history/force-app/main/default/lwc/hotelAppHomeRoomDetails/hotelAppHomeRoomDetails_20220609060126.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement,wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
-
-export default class HotelAppHomeRoomDetails extends LightningElement {
-
-
-
-}
